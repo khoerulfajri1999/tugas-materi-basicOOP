e4339e44b4656e8d595837c0eb04a25ce0ab3bdd
khoerulfajri1999 <khoerulfajri1999@gmail.com>
2023-03-02 17:15:51 +0700

    Membuat file index.html dan javascript mini library

---
+++ basicOOP.js
@@ -0,0 +1,8 @@
+new gridjs.Grid({ 
+  columns: ['Name', 'Email'],
+  data: [
+    ['Khoerul Fajri', 'khoerulfajri1999@gmail.com'],
+    ['Neni Maryamah', 'nenimaryamah123@gmail.com'],
+    ['Putri Auliya', 'auliyaputri11@gmail.com']
+  ] 
+}).render(document.getElementById('table'));
