22c1970e6d587ef7d3fb3bee34a7c13410b80305
khoerulfajri1999 <khoerulfajri1999@gmail.com>
2023-03-07 02:05:17 +0700

    Merevisi kodingan pada basicOOP.js dan index.html

--- basicOOP.js
+++ basicOOP.js
@@ -1,8 +1,51 @@
-new gridjs.Grid({ 
-  columns: ['Name', 'Email'],
+class Table {
+  constructor(init) {
+    this.init = init;
+  }
+
+  buatHeader(data) {
+    let open = "<thead><tr>";
+    let close = "</tr></thead>";
+    data.map((d) => {
+      open += `<th>${d}</th>`;
+    });
+
+    return open + close;
+  }
+
+  isi(data) {
+    let open = "<tbody>";
+    let close = "</tbody>";
+
+    data.map((d) => {
+      open += `
+        <tr>
+          <td>${d[0]}</td>
+          <td>${d[1]}</td>
+        </tr>
+      `;
+    });
+
+    return open + close;
+  }
+
+  render(element) {
+    let table =
+      "<table class='table table-hover'>" +
+      this.buatHeader(this.init.columns) +
+      this.isi(this.init.data) +
+      "</table>";
+    element.innerHTML = table;
+  }
+}
+
+const table = new Table({
+  columns: ["Name", "Email"],
   data: [
     ['Khoerul Fajri', 'khoerulfajri1999@gmail.com'],
     ['Neni Maryamah', 'nenimaryamah123@gmail.com'],
     ['Putri Auliya', 'auliyaputri11@gmail.com']
-  ] 
-}).render(document.getElementById('table'));
+  ]
+});
+const tabel = document.getElementById("tabel");
+table.render(tabel);
